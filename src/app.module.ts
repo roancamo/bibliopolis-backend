b1c8aadc7d0ef32229cfb336dbffc6e1eaa96797
roancamo <rodrigo.cabello.1979@gmail.com>
2024-07-25 21:31:34 -0400

    inicio_pedido

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,11 @@ import { UsuariosController } from './usuarios/usuarios.controller';
 import { UsuariosService } from './usuarios/usuarios.service';
 import { LibrosController } from './libros/libros.controller';
 import { LibrosService } from './libros/libros.service';
+import { PedidosController } from './pedidos/pedidos.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, UsuariosController, LibrosController],
+  controllers: [AppController, UsuariosController, LibrosController, PedidosController],
   providers: [AppService, UsuariosService, LibrosService],
 })
 export class AppModule {}
